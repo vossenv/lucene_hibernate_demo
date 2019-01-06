2bb04bf8a1bb98e72a6921426386125a39c15299
jansseneda-adobe <danimaejanssen@gmail.com>
2019-01-05 23:37:54 -0600

    initial

---
+++ src/main/java/com/dm/teamquery/TeamqueryApplication.java
@@ -0,0 +1,14 @@
+package com.dm.teamquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamqueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamqueryApplication.class, args);
+	}
+
+}
+
