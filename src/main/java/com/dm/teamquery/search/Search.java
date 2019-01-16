6647dc64a0eaeaf198687282314ef0b5cef13bb2
jansseneda-adobe <danimaejanssen@gmail.com>
2019-01-15 23:02:32 -0600

    ready to implement

--- src/main/java/com/dm/teamquery/search/Search.java
+++ src/main/java/com/dm/teamquery/search/Search.java
@@ -14,6 +14,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+import static java.util.Arrays.asList;
+import static java.util.Arrays.stream;
+
 @Getter
 @Setter
 @EqualsAndHashCode
@@ -35,11 +38,15 @@ public class Search {
     private Set<String> toProcess = new HashSet<>();
     private SearchEntity searchEntity;
 
+    // TEMP
+    private final static String queryKey = "query";
+    private final static String processKey = "toProcess";
+    private final static String andKey = "and";
+    public final static String termKey = "terms";
+
     public Search(Class entityType, String query, Pageable page) {
-        this.searchEntity = new SearchEntity(query);
-        initializeType(entityType);
-        initializeQuery(query);
         this.page = page;
+        initializeType(entityType);
     }
 
     public Search(Class type, String query) {
@@ -47,13 +54,16 @@ public class Search {
     }
 
     private void initializeQuery(String query){
+        this.searchEntity = new SearchEntity(query);
         this.query = query;
         this.currentQuery = query;
+        indexTerms();
     }
 
     private void initializeType(Class entityType){
         this.entityType = entityType;
         this.fieldNames = Arrays.stream(entityType.getDeclaredFields()).map(Field::getName).collect(Collectors.toSet());
+        initializeQuery(this.query);
     }
 
     public void setQuery(String query) {
@@ -64,8 +74,50 @@ public class Search {
         initializeType(entityType);
     }
 
+    // the main method
+    private void indexTerms(){
+        this.currentQuery = this.query;
+        booleanFilter();
+
+    }
+
+    private void booleanFilter(){
+
+
+        System.out.println();
+
+    }
+
+    private void booleanFilter1(Map<String, List<String>> searchPatterns) {
+
+        asList(searchPatterns.get(queryKey).get(0).split(OR_OPERATOR)).forEach(p -> {
+            String[] adds = (p.trim()).split(AND_OPERATOR);
+            if (adds.length > 1)
+                searchPatterns.get(andKey).add(stream(adds).map(String::trim).collect(Collectors.joining(AND_OPERATOR)));
+            else
+                searchPatterns.get(processKey).add(p.trim());
+        });
+
+        List<String> newAndList = new LinkedList<>();
+
+        searchPatterns.get(andKey).forEach(val -> {
+            if (val.matches(".*\\s+.*")){
+                List<String> keyQuoted = match(keyTermQuoted, val);
+                List<String> quoted = match(quotedSearchPattern, val);
+                for (String q : keyQuoted) val = swapSpace(val, q);
+                for (String q : quoted) val = swapSpace(val, q);
+                newAndList.addAll(restoreSpace(val));
+            } else {
+                newAndList.add(val);
+            }
+        });
+
+        searchPatterns.put(andKey, newAndList.stream().filter(v -> !v.isEmpty()).collect(Collectors.toList()));
+    }
+
+
     private String swapSpace (String string, String term){
-        return  string.replaceAll(term, term.replaceAll("\\s+","__").replace("\"",""));
+        return string.replaceAll(term, term.replaceAll("\\s+","__").replace("\"",""));
     }
 
     private List<String> restoreSpace (String string){
