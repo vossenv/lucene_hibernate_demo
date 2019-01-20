b71b1d64e49dc625957bf55ea22e9080a6d59128
jansseneda-adobe <danimaejanssen@gmail.com>
2019-01-20 00:16:42 -0600

    postman moore!

--- src/main/java/com/dm/teamquery/Execption/BadEntityException.java
+++ src/main/java/com/dm/teamquery/Execption/BadEntityException.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import java.util.ArrayList;
 import java.util.List;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class BadEntityException extends Exception {
 
 
