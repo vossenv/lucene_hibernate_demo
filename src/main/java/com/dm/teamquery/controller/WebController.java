4999e74e25a2295a9fe5351e70b1a380c87b0b77
jansseneda-adobe <danimaejanssen@gmail.com>
2019-01-20 02:58:33 -0600

    configure for deploy!

--- src/main/java/com/dm/teamquery/controller/WebController.java
+++ src/main/java/com/dm/teamquery/controller/WebController.java
@@ -3,6 +3,7 @@ package com.dm.teamquery.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WebController {
@@ -12,4 +13,9 @@ public class WebController {
         return "index.html";
     }
 
+    @ResponseBody
+    @RequestMapping(value = {"/status"}, method = RequestMethod.GET)
+    public String status() {
+        return "true";
+    }
 }
