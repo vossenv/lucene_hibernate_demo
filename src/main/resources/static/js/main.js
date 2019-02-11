d96f3906328e0dd8653f2d0558bdc20fa764cb03
jansseneda-adobe <danimaejanssen@gmail.com>
2019-02-11 08:15:49 -0600

    first search

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,12 +1,31 @@
-$(document).ready(function(){
+"use strict";
+jQuery(function () {
 
-   console.log("helllo")
 
+
+    $( "#search-button" ).click(function() {
+        doSearch();
+    });
 
 });
 
 
+function doSearch(){
 
+
+    $.ajax({
+        url: "/challenges/search",
+        type: "GET",
+        beforeSend: function(xhr){xhr.setRequestHeader('query', $("#search-bar").val());},
+        success: function(data) {
+
+            console.log(data);
+            $("#results").text(JSON.stringify(data))
+
+        }
+    });
+
+}
 
 
 
