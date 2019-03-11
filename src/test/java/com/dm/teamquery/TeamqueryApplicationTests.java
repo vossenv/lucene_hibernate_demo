dbd7b0c90e55e6026840b223181c5a3fb27be780
Unknown <danimaejanssen@gmail.com>
2019-03-11 08:56:57 -0500

    play with abstract extension

--- src/test/java/com/dm/teamquery/TeamqueryApplicationTests.java
+++ src/test/java/com/dm/teamquery/TeamqueryApplicationTests.java
@@ -1,22 +1,42 @@
 package com.dm.teamquery;
 
 
+import com.dm.teamquery.data.repository.ChallengeRepository;
+import com.dm.teamquery.data.repository.ChallengeService;
+import com.dm.teamquery.data.repository.custom.ABsTest;
+import com.dm.teamquery.entity.Challenge;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.inject.Inject;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TeamqueryApplicationTests {
 
 
+    @Inject
+    ChallengeService challengeService;
 
-	@Test
-	public void contextLoads(){
+    @Inject
+    ABsTest ttt;
 
+    @Inject
+    ChallengeRepository challengeRepository;
 
+    @Test
+    public void contextLoads() {
 
-	}
+        ttt.setSimpleJpaRepository(challengeRepository);
 
+        Challenge c = new Challenge();
+        c.setAuthor("Carag");
+        c.setAnswer("What is the question");
+        c.setQuestion("What is the answer");
+        ttt.test(c);
+
+    }
 }
