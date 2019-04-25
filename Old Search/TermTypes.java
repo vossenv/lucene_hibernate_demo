212fae360bf490694d457762b917df44181d301d
Unknown <danimaejanssen@gmail.com>
2019-04-25 15:05:49 -0500

    cleanup_phase1

--- Old Search/TermTypes.java
+++
@@ -1,9 +0,0 @@
-//package com.dm.teamquery.search;
-//
-//public enum TermTypes {
-//    TEXT, QUOTED, KEYWORD, AND, OR;
-//
-//    public boolean isBoolean(){
-//        return this == AND || this == OR;
-//    }
-//}
